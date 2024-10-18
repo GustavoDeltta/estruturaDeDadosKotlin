0ff9a554c530c81455c944202674f6f296fa528e
Deltta <deltta.dev@gmail.com>
2024-10-18 12:53:08 -0300

    Mais algumas questões de funções recursivas realizadas.

--- funcoesRecursivas/execiseQuestions.kt
+++ funcoesRecursivas/execiseQuestions.kt
@@ -1,6 +1,14 @@
 package funcoesRecursivas
 
-// Crie uma função recursiva que calcule e retorne o fatorial de um número inteiro N.
+// Implemente uma função recursiva que, dado um número inteiro N, mostre a sequência decrescente de N até 1. (Concluída!)
+fun showDescending(value: Int) {
+    if (value!=0){
+        print("$value ")
+        showDescending(value-1)
+    }
+}
+
+// Crie uma função recursiva que calcule e retorne o fatorial de um número inteiro N. (Concluída!)
 fun fatorial(value: Int): Int{
     var response = value
     if (value != 1){
@@ -10,6 +18,10 @@ fun fatorial(value: Int): Int{
 }
 
 fun main(){
-    val response = fatorial(4)
-    println(response)
+
+    println("O fatorial de 4 é: ${fatorial(4)}")
+
+    print("A ordem decrescente de 6 até 1 é: ")
+    showDescending(6)
+
 }
